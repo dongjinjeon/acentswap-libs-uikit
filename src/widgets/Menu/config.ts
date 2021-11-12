97fb15915791cd868573c5419f39c72cdd5ab986
unknown <webcgi@naver.com>
2021-11-12 10:23:49 +0900

    1.0.0

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,195 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://exchange.acentswap.finance",
+      },
+      {
+        label: "Liquidity",
+        href: "https://exchange.acentswap.finance/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+  {
+    label: "Lottery",
+    icon: "TicketIcon",
+    href: "/lottery",
+  },
+  {
+    label: "NFT",
+    icon: "NftIcon",
+    href: "/nft",
+  },
+  {
+    label: "Profile & Teams",
+    icon: "GroupsIcon",
+    items: [
+      {
+        label: "Leaderboard",
+        href: "/teams",
+      },
+      {
+        label: "YourProfile",
+        href: "/",
+      },
+    ],
+    calloutClass: "rainbow",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://acentswap.info",
+      },
+      {
+        label: "Tokens",
+        href: "https://acentswap.info/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://acentswap.info/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://acentswap.info/accounts",
+      },
+    ],
+  },
+  {
+    label: "IFO",
+    icon: "IfoIcon",
+    items: [
+      {
+        label: "Next",
+        href: "/ifo",
+      },
+      {
+        label: "History",
+        href: "/ifo/history",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      {
+        label: "Voting",
+        href: "https://voting.acentswap.finance",
+      },
+      {
+        label: "Github",
+        href: "https://github.com/acentswap",
+      },
+      {
+        label: "Docs",
+        href: "https://docs.acentswap.finance",
+      },
+      {
+        label: "Blog",
+        href: "https://acentswap.medium.com",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  // {
+  //   label: "Telegram",
+  //   icon: "TelegramIcon",
+  //   items: [
+  //     {
+  //       label: "English",
+  //       href: "https://t.me/acentswap",
+  //     },
+  //     {
+  //       label: "Bahasa Indonesia",
+  //       href: "https://t.me/acentswapIndonesia",
+  //     },
+  //     {
+  //       label: "中文",
+  //       href: "https://t.me/acentswap_CN",
+  //     },
+  //     {
+  //       label: "Tiếng Việt",
+  //       href: "https://t.me/acentswapVN",
+  //     },
+  //     {
+  //       label: "Italiano",
+  //       href: "https://t.me/acentswap_ita",
+  //     },
+  //     {
+  //       label: "русский",
+  //       href: "https://t.me/acentswap_ru",
+  //     },
+  //     {
+  //       label: "Türkiye",
+  //       href: "https://t.me/acentswapturkiye",
+  //     },
+  //     {
+  //       label: "Português",
+  //       href: "https://t.me/acentswapPortuguese",
+  //     },
+  //     {
+  //       label: "Español",
+  //       href: "https://t.me/acentswapEs",
+  //     },
+  //     {
+  //       label: "日本語",
+  //       href: "https://t.me/acentswapjp",
+  //     },
+  //     {
+  //       label: "Français",
+  //       href: "https://t.me/acentswapfr",
+  //     },
+  //     {
+  //       label: "Announcements",
+  //       href: "https://t.me/acentswapAnn",
+  //     },
+  //     {
+  //       label: "Whale Alert",
+  //       href: "https://t.me/acentswapWhales",
+  //     },
+  //   ],
+  // },
+
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    href: "https://t.me/TechnologyCoin",
+    items: [],
+  },
+
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/coin_technology",
+    items: [],
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+// export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_FULL = 0;
+export const SIDEBAR_WIDTH_REDUCED = 56;
